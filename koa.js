9986ad25db1e2561b12685bc16787bb78f46ac64
lijiaxu <lijiaxu@ufsoft.com.cn>
2016-09-23 15:51:18 +0800

    结构优化，地址栏可直接访问文件夹/文件名

--- koa.js
+++ koa.js
@@ -1,32 +1,29 @@
-
 var send = require('koa-send');
 var Koa = require('koa');
+var serve = require('koa-static');
 var app = new Koa();
 var Router = require('koa-router');
-var cors = require('koa-cors');
-var koaBody = require('koa-body')();
 var gzip = require('koa-gzip');
  var cacheControl = require('koa-cache-control');
 var router = new Router({
     prefix: '/public'
 });
-
-app.use(gzip());
-app.use(cors())
-    .use(router.routes())
-    .use(router.allowedMethods());
-    router.get('/:fname/:cname', koaBody, function*(next) {
-            var fname = this.params.fname;
-            var cname = this.params.cname;
-            console.log("文件类型" + fname);
-            console.log("文件名" + cname);
-          yield send(this, "./public/" + fname + "/" + cname);
-            yield next;
-        })
+// app.use(cors())
+//     .use(router.routes())
+//     .use(router.allowedMethods());
+    // router.get('/:fname/:cname', koaBody, function*(next) {
+    //         var fname = this.params.fname;
+    //         var cname = this.params.cname;
+    //         console.log("文件类型" + fname);
+    //         console.log("文件名" + cname);
+    //       yield send(this, "./public/" + fname + "/" + cname);
+    //         yield next;
+    //     })
     app.use(cacheControl({
     maxAge: 500
 }));
     app.use(function *(next){
+
         this.cacheControl = {
             maxAge: 60000
         };
@@ -35,7 +32,7 @@ app.use(cors())
             yield next;
         } catch (err) {
             this.cacheControl = {
-                maxAge: 5
+                maxAge: 50
             };
         }
     });
@@ -44,11 +41,11 @@ app.use(function*(next){
   // cache is ok
   if (this.fresh) {
     this.status = 304;
-    //  yield header('Cache-Control', 'max-age=604800');
   }
 yield next;
 })
-
+app.use(gzip());
+app.use(serve(__dirname + '/public'));
 var port = 8003;
 app.listen(port);
 console.log(` 启动成功,端口: ${port}`);
