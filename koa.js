56694e6bc70e18bf7376aa494c9ec27e0f9d9def
lijiaxu <lijiaxu@ufsoft.com.cn>
2016-09-23 13:48:10 +0800

    修复304与gzip冲突

--- koa.js
+++ koa.js
@@ -1,6 +1,4 @@
-// var http = require('http');
-// var url = require("url");
-// var path = require("path");
+
 var send = require('koa-send');
 var Koa = require('koa');
 var app = new Koa();
@@ -13,68 +11,45 @@ var staticCache = require('koa-static-cache');
 var router = new Router({
     prefix: '/public'
 });
-var files = {};
-// app.use(staticCache('/public', {
-//     maxAge: 60 * 60 * 24 * 365
-// },files)
-// files['/package.json'].maxAge = 60 * 60 * 24 * 30
+
 app.use(gzip());
 app.use(cors())
     .use(router.routes())
     .use(router.allowedMethods());
-
-// var server = http.createServer(function(request, response) {
-//     var pathname = url.parse(request.url).pathname;
-//     var realPath = "assets" + pathname;
-//     console.log(realpath);
-//     fs.readFile(realpath, "binary", function(err, file) {
-//         if (err) {
-//             res.writeHead(500, {
-//                 'Content-type': 'text/plain'
-//             })
-//             res.end(err)
-//         } else {
-//             res.writeHead(304, {
-//                 'Access-Control-Allow-Origin': '*',
-//                 'Content-Type': 'image/png',
-//                 'ETag': "666666",
-//                 'Cache-Control': 'max-age=31536000, public',
-//                 'Expires': 'Mon, 07 Sep 2026 09:32:27 GMT'
-//             })
-//             res.write(file, "binary")
-//             res.end()
-//         }
-//     })
-// })
-
-router.get('/:fname/:cname', koaBody, function*() {
-        var fname = this.params.fname;
-        var cname = this.params.cname;
-        console.log("文件类型" + fname);
-        console.log("文件名" + cname);
-        // res.header('Cache-Control', 'max-age=604800')
-        yield send(this, "./public/" + fname + "/" + cname);
-    })
-    // app.use(function*(req, res, next) {
-    //     res.cacheControl({
-    //         maxAge: 500000
-    //     });
-    //     next();
-    // });
-app.use(function*(next) {
-    this.cacheControl = {
-        maxAge: 6000
-    };
-
-    try {
-        yield next;
-    } catch (err) {
+    router.get('/:fname/:cname', koaBody, function*(next) {
+            var fname = this.params.fname;
+            var cname = this.params.cname;
+            console.log("文件类型" + fname);
+            console.log("文件名" + cname);
+          yield send(this, "./public/" + fname + "/" + cname);
+            yield next;
+        })
+    app.use(cacheControl({
+    maxAge: 500
+}));
+    app.use(function *(next){
         this.cacheControl = {
-            maxAge: 500
+            maxAge: 60000
         };
-    }
-});
+
+        try {
+            yield next;
+        } catch (err) {
+            this.cacheControl = {
+                maxAge: 5
+            };
+        }
+    });
+app.use(function*(next){
+  this.set('ETag', '123');
+  // cache is ok
+  if (this.fresh) {
+    this.status = 304;
+    //  yield header('Cache-Control', 'max-age=604800');
+  }
+yield next;
+})
+
 var port = 8003;
 app.listen(port);
-
 console.log(` 启动成功,端口: ${port}`);
