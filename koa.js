636e3c784575c7ebcc8629233741a08cc219641c
lijiaxu <lijiaxu@ufsoft.com.cn>
2016-09-23 16:19:51 +0800

    实时监测空静态文件操作,需手动安装 npm i chokidar

--- koa.js
+++ koa.js
@@ -5,6 +5,7 @@ var app = new Koa();
 var Router = require('koa-router');
 var gzip = require('koa-gzip');
  var cacheControl = require('koa-cache-control');
+ var chokidar = require('chokidar');
 var router = new Router({
     prefix: '/public'
 });
@@ -46,6 +47,25 @@ yield next;
 })
 app.use(gzip());
 app.use(serve(__dirname + '/public'));
+
 var port = 8003;
 app.listen(port);
 console.log(` 启动成功,端口: ${port}`);
+var watcher = chokidar.watch("public", {
+    persistent: true // 保持监听状态
+});
+// 监听增加，修改，删除文件的事件
+watcher.on('all', (event, path) => {
+    switch (event) {
+    case 'add':
+        console.log('添加文件'+path)
+    case 'change':
+        console.log('改变文件'+path)
+        break;
+    case 'unlink':
+        console.log('删除文件'+path);
+        break;
+    default:
+        break;
+    }
+});
