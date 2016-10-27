b30b18e7a05d32fbcb2b28e478dbeb2f5b14a400
yezhang <yezhang989@163.com>
2016-10-27 20:22:05 +0800

    添加了微信路由

--- servers/wx/configWxServer.js
+++ servers/wx/configWxServer.js
@@ -9,8 +9,36 @@ module.exports = function (hostConfig, router) {
     const indexPagePath = hostConfig.Path + '/index.html';
     console.log(indexPagePath)
 
-    router.get('/wx', function* (next) {
-        console.log('收到了请求')
+    /**
+     * 我的发票
+     */
+    router.get('/wechatInvoice', function* (next) {
+        console.log('/wechatInvoice')
         yield send(this, indexPagePath)
-    })
+    });
+
+    /**
+     * 报销列表
+     */
+    router.get('/wechatReimbursement', function* (next) {
+        console.log('/wechatReimbursement')
+        yield send(this, indexPagePath)
+    });
+
+    /**
+     * 创建报销
+     */
+    router.get('/createReimbursement', function* (next) {
+        console.log('/createReimbursement')
+        yield send(this, indexPagePath)
+    });
+
+    /**
+     * 发票名片
+     */
+    router.get('/card', function* (next) {
+        console.log('/card')
+        yield send(this, indexPagePath)
+    });
+
 }
